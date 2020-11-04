de85734c36f73f4e5a43b426c2d25199a8a1cfc6
Himanshu-codes <73941827+Himanshu-codes@users.noreply.github.com>
2020-11-04 17:14:40 +0530

    Add files via upload

---
+++ ascii value.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+char c;
+printf("enter character");
+scanf("%c",&c);
+printf("\nASCII value of %c is %d",c,c);
+}
---
+++ himanhsu name.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+char name[10];
+printf("Enter your name");
+scanf("%s",&name);
+printf("HELLO! How are you  %s",name);
+}
